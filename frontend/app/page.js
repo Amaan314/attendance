c7e0ee7a75e65255a3bc162d211cb927931dbf1a
Amaan314 <gtatri266@gmail.com>
2025-09-07 18:52:50 +0530

    sdadskndk

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,25 +1,6 @@
 // app/page.js
-
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/login");
-  }, [router]);
-
-  // Display a message while the redirection is in progress
-  return (
-    <div>
-      <h1>Redirecting...</h1>
-      <p>
-        If you are not redirected automatically, <a href="/login">click here</a>
-        .
-      </p>
-    </div>
-  );
+  redirect("/login");
 }
