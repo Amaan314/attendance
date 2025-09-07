a657a78d197a1add8718dbc176f82af1bfa71842
Amaan314 <gtatri266@gmail.com>
2025-09-07 20:05:19 +0530

    whatever

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,6 @@
-// // app/page.js
-// import { redirect } from "next/navigation";
-
-// export default function HomePage() {
-//   redirect("/login");
-// }
-
 // app/page.js
-export default function Home() {
-  return <div>Home Page</div>;
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/login");
 }
